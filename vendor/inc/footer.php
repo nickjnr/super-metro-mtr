8b2ce39dcc08166e604053f4f7440e280af5d67e
nickjnr <nickmuinde50@gmail.com>
2024-12-05 09:42:20 +0300

    commit

--- vendor/inc/footer.php
+++ vendor/inc/footer.php
@@ -1,6 +1,6 @@
 <footer class="py-5 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; <?php echo date('Y');?> Vehicle Booking System | Crafted With <i class ="fa fa-heartbeat"></i> By Martin Mbithi Nzilani   
+      <p class="m-0 text-center text-white">Copyright &copy; <?php echo date('Y');?> Vehicle Booking System | Crafted With <i class ="fa fa-heartbeat"></i> By Nicholas Muinde  
        </p>
     </div>
     <!-- /.container -->
